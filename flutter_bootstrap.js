fed08f05103cfcdca1083d4697017a82a6326edd
John-Cho <70558251+John-Cho@users.noreply.github.com>
2025-01-02 16:22:12 +0900

    new pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3411128545"
+    serviceWorkerVersion: "3761722769"
   }
 });
